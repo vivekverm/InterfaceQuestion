3ace835812252b83c6a303141de3dda4456f8855
Vivek kumar <89844805+vivekverm@users.noreply.github.com>
2021-10-08 17:59:42 +0530

    Create Veena.java

---
+++ Veena.java
@@ -0,0 +1,12 @@
+package Music;
+interface Playable
+{
+    public void play();
+}
+public class Veena implements Playable {
+    public void play()
+    {
+        System.out.println("Welcome to java world");
+    }
+
+}
